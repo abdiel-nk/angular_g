fb6dd7101dabc1325318c9966b381c27ae2d687c
unknown <abdielmamani@upeu.edu.pe>
2024-09-05 16:02:24 -0500

    last change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { DashboardComponent } from './dashboard/component/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgIf,RouterOutlet, TestComponent,DashboardComponent],
+  imports: [NgIf,RouterOutlet, TestComponent,DashboardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular0';
+  condition =true;
+  message='from app componente'
+  handleClick(){
+    this.message = 'clicked at the app component';
+  }
 }
