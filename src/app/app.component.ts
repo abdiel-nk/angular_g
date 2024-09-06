f44689cdb0c9ab3d32c3ee4fff84708f76f954aa
AbdielMamani <abdielmamani@upeu.edu.pe>
2024-09-05 20:36:43 -0500

    ng-template & ng-container & ng-template-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { TestComponent } from './test/test.component';
 import { CommonModule, NgIf } from '@angular/common';
 import { DashboardComponent } from './dashboard/component/dashboard.component';
 
+interface arrayItem{
+  id:number;
+  name:string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,10 +16,21 @@ import { DashboardComponent } from './dashboard/component/dashboard.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Angular0';
-  condition =true;
-  message='from app componente'
+  condition = true;
+  selectedOption = 'tres';
+  options = ['uno','dos','tres'];
+  array=[
+    {id:1,name: 'item1' },
+    {id:2,name: 'item2' },
+
+  ];
+  trackByFn(item: any){
+    return item.id
+  }
+  message='from app component'
   handleClick(){
     this.message = 'clicked at the app component';
   }
